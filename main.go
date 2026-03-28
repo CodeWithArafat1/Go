9f0992d249df7db44b2ba2ecd42d655e53d70bb8
codewitharafat1 <arafatnill2.0@gmail.com>
2026-03-29 05:08:30 +0600

    create custom type using struct

--- main.go
+++ main.go
@@ -42,11 +42,47 @@ import "fmt"
 
 //! task 2
 
+// func main() {
+// 	ammo := 10
+// 	ammoPtr := &ammo // Memory Address Access
+// 	fmt.Println("Original Ammo: ", ammo)
+// 	fmt.Println("Memory Address: ", ammoPtr)
+// 	*ammoPtr = 999 // Inter the Memory and change value (hidden)
+// 	fmt.Println("Hacked Ammo: ", ammo)
+// }
+
+// type Hacker struct {
+// 	Codename string
+// 	level    int
+// 	IsActive bool
+// }
+
+// func main() {
+// 	user1 :=
+// 		Hacker{
+// 			Codename: "Nill",
+// 			level:    99,
+// 			IsActive: true,
+// 		}
+// 	fmt.Printf("Hacker Name: %s\n", user1.Codename)
+// 	fmt.Printf("Level: %d\n", user1.level)
+// 	fmt.Printf("Level: %t\n", user1.IsActive)
+// }
+
+//! task 3
+
+type Target struct {
+	IP           string
+	Port         int
+	IsVulnerable bool
+}
+
 func main() {
-	ammo := 10
-	ammoPtr := &ammo // Memory Address Access
-	fmt.Println("Original Ammo: ", ammo)
-	fmt.Println("Memory Address: ", ammoPtr)
-	*ammoPtr = 999 // Inter the Memory and change value (hidden)
-	fmt.Println("Hacked Ammo: ", ammo)
+	target1 := Target{
+		IP:           "192.168.1.5",
+		Port:         22,
+		IsVulnerable: true,
+	}
+
+	fmt.Printf("IP: %s, Port: %d IsVulnerable: %t", target1.IP, target1.Port, target1.IsVulnerable)
 }
